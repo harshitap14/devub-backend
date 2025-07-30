a26b95d20afa5a8695bb5251e1841ddc96689ec9
harshitap14 <harshita.p@gleecus.com>
2025-07-30 16:49:56 +0530

    Update urls.py

--- devhub/devhub/urls.py
+++ devhub/devhub/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
-from devhubapp.api_views import GitHubCallbackView
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -18,7 +17,6 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("devhubapp.urls")),
-    path("callback/", GitHubCallbackView.as_view(), name='github_callback'),
     # Swagger/Redoc
     path("swagger/", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path("redoc/", schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
