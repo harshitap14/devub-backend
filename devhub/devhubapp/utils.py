0684187dd889298d517ec29eed17b7438fe6f03a
harshitap14 <harshita.p@gleecus.com>
2025-07-30 16:07:34 +0530

    Update utils.py

--- devhub/devhubapp/utils.py
+++ devhub/devhubapp/utils.py
@@ -1,1 +1,29 @@
+from django.contrib.auth.models import User
+
+def get_or_create_shadow_user_for_appuser(appuser):
+    user, created = User.objects.get_or_create(
+        username=appuser.email,
+        defaults={
+            "first_name": appuser.first_name,
+            "last_name": appuser.last_name,
+            "email": appuser.email,
+        }
+    )
+
+    # Optional: update user info in case AppUser info changed
+    if not created:
+        updated = False
+        if user.first_name != appuser.first_name:
+            user.first_name = appuser.first_name
+            updated = True
+        if user.last_name != appuser.last_name:
+            user.last_name = appuser.last_name
+            updated = True
+        if user.email != appuser.email:
+            user.email = appuser.email
+            updated = True
+        if updated:
+            user.save()
+
+    return user
 
